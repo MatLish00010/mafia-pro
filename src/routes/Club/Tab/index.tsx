8ad4faff150fd9dd14cee8007c88296358b18a97
Матвей Лишай <neamico2022@gmail.com>
2024-05-01 18:30:32 +0200

    save selected tabs in url params

--- src/routes/Club/Tab/index.tsx
+++ src/routes/Club/Tab/index.tsx
@@ -1,3 +1,5 @@
+import { useSearchParams } from "react-router-dom";
+
 import Table from "@/routes/Rating/Table";
 import type { Tables } from "@/types/supabase.ts";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui/tabs.tsx";
@@ -10,12 +12,33 @@ interface Props {
 }
 
 export default function Tab({ club }: Props) {
+	const [searchParams, setSearch] = useSearchParams();
+
+	const handleClick = (tab: string) => {
+		setSearch(
+			{
+				club: searchParams.get("club") || "",
+				segment: tab,
+			},
+			{ replace: true },
+		);
+	};
+
 	return (
-		<Tabs defaultValue="players" className="flex flex-col items-center">
+		<Tabs
+			defaultValue={searchParams.get("segment") || "players"}
+			className="flex flex-col items-center"
+		>
 			<TabsList className="max-w-max">
-				<TabsTrigger value="players">Players</TabsTrigger>
-				<TabsTrigger value="games">Games</TabsTrigger>
-				<TabsTrigger value="rating">Rating</TabsTrigger>
+				<TabsTrigger value="players" onClick={() => handleClick("players")}>
+					Players
+				</TabsTrigger>
+				<TabsTrigger value="games" onClick={() => handleClick("games")}>
+					Games
+				</TabsTrigger>
+				<TabsTrigger value="rating" onClick={() => handleClick("rating")}>
+					Rating
+				</TabsTrigger>
 			</TabsList>
 			<TabsContent value="players" className="w-full">
 				<Players club_id={club.id} />
--- src/routes/Club/index.tsx
+++ src/routes/Club/index.tsx
@@ -1,3 +1,5 @@
+import { useSearchParams } from "react-router-dom";
+
 import Tab from "@/routes/Club/Tab";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui/tabs.tsx";
 
@@ -5,14 +7,32 @@ import useClub from "./hooks/useClub.ts";
 
 export default function Club() {
 	const { clubs } = useClub();
+	const [searchParams, setSearch] = useSearchParams();
+
+	const handleClick = (id: string) => {
+		setSearch(
+			{
+				club: id,
+				segment: searchParams.get("searchParams") || "",
+			},
+			{ replace: true },
+		);
+	};
 
 	return (
 		<section>
 			{!!clubs?.length && (
-				<Tabs defaultValue={clubs[0].id} className="flex flex-col items-center">
+				<Tabs
+					defaultValue={searchParams.get("club") || clubs[0].id}
+					className="flex flex-col items-center"
+				>
 					<TabsList className="max-w-max">
 						{clubs.map((club) => (
-							<TabsTrigger key={club.id} value={club.id}>
+							<TabsTrigger
+								key={club.id}
+								value={club.id}
+								onClick={() => handleClick(club.id)}
+							>
 								{club.name}
 							</TabsTrigger>
 						))}
