fac4d1a8e452d1f916620cdb2037fe062a1a7e1c
Матвей Лишай <neamico2022@gmail.com>
2024-05-02 11:20:46 +0200

    fix seve selected tabs in url

--- src/routes/Club/Tab/index.tsx
+++ src/routes/Club/Tab/index.tsx
@@ -4,6 +4,7 @@ import Table from "@/routes/Rating/Table";
 import type { Tables } from "@/types/supabase.ts";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui/tabs.tsx";
 
+import { useEffect } from "react";
 import Games from "./Games";
 import Players from "./Players";
 
@@ -14,7 +15,7 @@ interface Props {
 export default function Tab({ club }: Props) {
 	const [searchParams, setSearch] = useSearchParams();
 
-	const handleClick = (tab: string) => {
+	const setParams = (tab: string) => {
 		setSearch(
 			{
 				club: searchParams.get("club") || "",
@@ -24,19 +25,25 @@ export default function Tab({ club }: Props) {
 		);
 	};
 
+	useEffect(() => {
+		if (!searchParams.get("segment")) {
+			setParams("players");
+		}
+	}, []);
+
 	return (
 		<Tabs
 			defaultValue={searchParams.get("segment") || "players"}
 			className="flex flex-col items-center"
 		>
 			<TabsList className="max-w-max">
-				<TabsTrigger value="players" onClick={() => handleClick("players")}>
+				<TabsTrigger value="players" onClick={() => setParams("players")}>
 					Players
 				</TabsTrigger>
-				<TabsTrigger value="games" onClick={() => handleClick("games")}>
+				<TabsTrigger value="games" onClick={() => setParams("games")}>
 					Games
 				</TabsTrigger>
-				<TabsTrigger value="rating" onClick={() => handleClick("rating")}>
+				<TabsTrigger value="rating" onClick={() => setParams("rating")}>
 					Rating
 				</TabsTrigger>
 			</TabsList>
--- src/routes/Club/index.tsx
+++ src/routes/Club/index.tsx
@@ -3,21 +3,28 @@ import { useSearchParams } from "react-router-dom";
 import Tab from "@/routes/Club/Tab";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui/tabs.tsx";
 
+import { useEffect } from "react";
 import useClub from "./hooks/useClub.ts";
 
 export default function Club() {
 	const { clubs } = useClub();
 	const [searchParams, setSearch] = useSearchParams();
 
-	const handleClick = (id: string) => {
+	const setParams = (id: string) => {
 		setSearch(
 			{
 				club: id,
-				segment: searchParams.get("searchParams") || "",
+				segment: searchParams.get("segment") || "",
 			},
 			{ replace: true },
 		);
 	};
+
+	useEffect(() => {
+		if (!searchParams.get("club") && clubs?.[0].id) {
+			setParams(clubs[0].id);
+		}
+	}, [clubs]);
 
 	return (
 		<section>
@@ -31,7 +38,7 @@ export default function Club() {
 							<TabsTrigger
 								key={club.id}
 								value={club.id}
-								onClick={() => handleClick(club.id)}
+								onClick={() => setParams(club.id)}
 							>
 								{club.name}
 							</TabsTrigger>
